a662a1f3cc7fdacce218dfc771d2defcaedab2b9
Tam <115720528+NguyenTam03@users.noreply.github.com>
2022-11-28 02:57:05 +0700

    Create Ramdom.bat

---
+++ Ramdom.bat
@@ -0,0 +1,6 @@
+@echo off
+:start
+echo Hacking .............................
+color 02
+echo %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% 
+goto start
